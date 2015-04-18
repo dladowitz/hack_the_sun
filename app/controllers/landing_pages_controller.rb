25397f2717b2a4b58864458c85481552acf1668f
David Ladowitz <david@ladowitz.com>
2015-04-18 11:33:16 -0700

    adding json to calc_savings

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -6,11 +6,18 @@ class LandingPagesController < ApplicationController
 
   def calc_savings
 
-    # Do calculations here
-    redirect_to savings_path
+    time_data = [[Time.now, 250, 350], [Time.now, 300, 400], [Time.now, 400, 500]]
+    @time_data_json = { data: time_data }.to_json
+    render layout: "landing_page/landing_layout"
   end
 
   def savings
     render layout: "landing_page/landing_layout"
   end
+
+
+
+  private
+
+
 end
